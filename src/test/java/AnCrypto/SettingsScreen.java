95ca7ca05a266fae9372dcefbbbca71a8bd40c33
ShivaAntier <shiva.mahajan@antiersolutions.com>
2023-02-15 14:39:44 +0530

    no changes

--- src/test/java/AnCrypto/SettingsScreen.java
+++ src/test/java/AnCrypto/SettingsScreen.java
@@ -43,7 +43,6 @@ public class SettingsScreen extends BaseClass {
     @Test(priority = 3,enabled = true)
     void OpenProfileScreen(){
         driver.findElement(By.xpath("//*[@class='android.widget.ScrollView']/android.view.ViewGroup/android.view.ViewGroup[1]")).click();
-        
     }
 
 }
